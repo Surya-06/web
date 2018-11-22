a099e45fdcafccaca3ca7f538c60bfc4a7f3bbbb
Surya Prakash Susarla <susarla.surya.prakash.1998@gmail.com>
2018-11-22 17:47:40 +0530

    Final commit before deployment

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,38 +1,118 @@
 import React , {Component} from 'react';
-import M from 'materialize-css';
 import '../stylesheets/materialize.css';
 import '../stylesheets/body.css';
 
-import background1 from '../res/background1.jpg';
-import background2 from '../res/background2.jpg';
+var acheivements = {
+    title: 'About Me',
+    first_word: 'Apart',
+    content: ' from the regular courses taken in college I am pursuing other interesting topics such as Machine Learning, Web Scraping and Image processing as well. I have maintained decent academic track record throughout my schooling and have been maintaing my CGPA above 9.I have attended workshops organised by various tech organisations, some of them are Internet of Things workshop oraganised by Mozilla Firefox and Google Mobile Developer Fest. I have also been selected in the Google Challenge India Scholarship for Andorid Development.Contributing to open source projects and knowing about cutting - edge trends is one of my favorite pastime activities.',
+};
 
+var projects = {
+    title : 'Projects',
+    first_word : 'Please',
+    content : ' visit my LinkedIn or GitHub accounts to view all my projects. Thank you :-)'
+};
+
+var contacts = {
+    title : 'Reach me at ',
+    first_word : 'Please ',
+    content : ' feel free to reach me at. Email : susarla.surya.prakash.1998@gmail.com',
+    email : 'susarla.surya.prakash.1998@gmail.com',
+    phone : '+91 888 555 8748'
+};
+
+var home = {
+    title : 'Welcome !',
+    first_word : 'Hello ! ',
+    content : 'I am Surya and welcome to my page.I am a Computer Programmer◉ Machine Learning Enthusiast◉ Android App Developer & Web Developer◉ Tech Buff ',
+    show_content : true 
+};
 
 class Body extends Component{
 
-    componentDidMount(){
-        var elems = document.querySelectorAll('.parallax');
-        M.Parallax.init(elems,{});
-    }
+    constructor(){
+        super();
+        this.state = {
+            title : 'Welcome !',
+            first_word : 'Hello ! ',
+            content : 'I am Surya and welcome to my page.I am a Computer Programmer◉ Machine Learning Enthusiast◉ Android App Developer & Web Developer◉ Tech Buff ',
+            show_content : true 
+        }
+    };
+
+    changeToAcheivements = () => {
+        this.setState({
+            title: acheivements.title,
+            first_word : acheivements.first_word,
+            content : acheivements.content,
+            show_content : true
+        });
+    };
+
+    changeToProjects = () => {
+        this.setState({
+            title : projects.title,
+            first_word : projects.first_word,
+            content : projects.content,
+            show_content : true 
+        });
+    };
+
+    changeToContacts = () => {
+        this.setState({
+            title : contacts.title,
+            first_word : contacts.first_word,
+            content : contacts.content,
+            show_content : false,
+            email : contacts.email,
+            phone : contacts.phone
+        });
+    };
+
+    changeToHome = () => {
+        this.setState({
+            title : home.title,
+            first_word : home.first_word,
+            content : home.content,
+            show_content : true 
+        });
+    };
 
     render(){
         return(
-            <div style={{display:'inline'}}>
+            <div>
                 <div className='parallax-container'>
-                    <div className='parallax'>
-                        <img src={background1} />
-                    </div>
+                    <div className='parallax'></div>
                 </div>
-                <div className='row borderText blue lighten-4' style={{margin:0}}>
-                    <div className='col s12 l6 offset-l3'>
-                        <div className='card-panel blue lighten-3 hoverable '>
-                            <h5 className='center-align'> SAMPLE TEXT </h5>
+                <div className='row'>
+                    <div className='col s12 l8 offset-l2'>
+                        <div className="card blue-grey darken-4 hoverable">
+                            <div className="card-content">
+                                <span className="card-title deep-orange-text center-align"><h2>{this.state.title}</h2></span>
+                                <div className='divider'></div>
+                                {
+                                    this.state.show_content ?
+                                        <p className = 'flow-text center-align'> <span style={{fontSize:'25pt'}}>{this.state.first_word}</span> {this.state.content} </p>
+                                    :
+                                    <div className='center-align'>
+                                        <a href='mailto : {this.state.email}' className='flow-text'> Email : {this.state.email} </a>
+                                        <br />
+                                        <a className='flow-text'> Phone : {this.state.phone} </a>
+                                    </div>
+                                }
+                            </div>
+                            <div className="card-action center-align">
+                                <a onClick={this.changeToAcheivements} className='flow-text'>Acheivements</a>
+                                <a onClick={this.changeToProjects} className='flow-text'>Projects</a>
+                                <a onClick={this.changeToContacts} className='flow-text'>Contact</a>
+                                <a onClick={this.changeToHome} className='flow-text'>Home</a>
+                            </div>
                         </div>
                     </div>
                 </div>
                 <div className='parallax-container'>
-                    <div className='parallax'>
-                        <img src={background2} />
-                    </div>
+                    <div className='parallax'></div>
                 </div>
             </div>
         );
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,6 +1,7 @@
 import React , {Component} from 'react';
 import M from 'materialize-css';
 import '../stylesheets/materialize.css';
+import '../stylesheets/navbar.css';
 
 class NavBar extends Component{
     componentDidMount(){
@@ -14,14 +15,14 @@ class NavBar extends Component{
                 <div className="nav-wrapper">
                     <div className="row">
                         <div className="col s12 m6">
-                            <a href="#" className="brand-logo center-align"><b>Surya Prakash Susarla</b></a>
+                            <a href="#" className="brand-logo center-align flow-text" >Surya Prakash Susarla</a>
                         </div>
                         <div className="col m6">
                             <ul className="right hide-on-med-and-down">
-                                <li><a href="" className="flow-text">Linked In</a></li>
-                                <li><a href="" className="flow-text">Github</a></li>
-                                <li><a href="" className="flow-text">Facebook</a></li>
-                                <li><a href="" className="flow-text">Résumé</a></li>
+                                <li><a href="https://linkedin.com/in/surya-prakash-susarla" target="_blank" className="flow-text">Linked In</a></li>
+                                <li><a href="https://github.com/Surya-06" target="_blank" className="flow-text">Github</a></li>
+                                <li><a href="https://www.facebook.com/ronaldo.alex.7545" target="_blank"  className="flow-text">Facebook</a></li>
+                                <li><a href="https://drive.google.com/file/d/1LhVsQHImLNHBN_EibQ8tqWzuygiqko0n/view?usp=sharing" target="_blank" className="flow-text">Résumé</a></li>
                             </ul>
                         </div>
                     <a href="" data-target="show-nav" className="sidenav-trigger left"><i className="material-icons">sort</i></a>
@@ -29,31 +30,37 @@ class NavBar extends Component{
                 </div>
             </nav>
             </div>
+            <div className='row'>
+                    <div className='col s9'>
             <ul className="sidenav" id="show-nav">
-                <li>
-                    < div className = "user-view  grey darken-3" ></div>
-                    <div className="section"></div>
-                </li>
-                <li>
-                    <a href="" className="flow-text center-align">Linked In</a>
-                    <div className="divider"></div>
-                    <div className="section"></div>
-                </li>
-                <li>
-                    <a href="" className="flow-text center-align">Github</a>
-                    <div className="divider"></div>
-                    <div className="section"></div>
-                </li>
-                <li>
-                    <a href="" className="flow-text center-align">Facebook</a>
-                    <div className="divider"></div>
-                    <div className="section"></div>
-                </li>
-                <li>
-                    <a href="" className="flow-text center-align">Résumé</a>
-                    <div className="divider"></div>
-                </li>
+                
+                        <li>
+                            < div className = "user-view  grey darken-3" ></div>
+                            <div className="section"></div>
+                        </li>
+                        <li>
+                            <a href="https://linkedin.com/in/surya-prakash-susarla" target="_blank" className="flow-text center-align">Linked In</a>
+                            <div className="divider"></div>
+                            <div className="section"></div>
+                        </li>
+                        <li>
+                            <a href="https://github.com/Surya-06" target="_blank" className="flow-text center-align">Github</a>
+                            <div className="divider"></div>
+                            <div className="section"></div>
+                        </li>
+                        <li>
+                            <a href="https://www.facebook.com/ronaldo.alex.7545" target="_blank"  className="flow-text center-align">Facebook</a>
+                            <div className="divider"></div>
+                            <div className="section"></div>
+                        </li>
+                        <li>
+                            <a href="https://drive.google.com/file/d/1LhVsQHImLNHBN_EibQ8tqWzuygiqko0n/view?usp=sharing" target="_blank" className="flow-text center-align">Résumé</a>
+                            <div className="divider"></div>
+                        </li>
             </ul>
+            
+                    </div>
+                </div>
             </div>
         );
     }
